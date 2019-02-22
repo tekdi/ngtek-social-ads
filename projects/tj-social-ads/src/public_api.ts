bb075ae7dd78f60caddfb651f73f3739589b464f
“your <sudhir_s@tekditechnologies.com>
2019-02-22 17:19:12 +0530

    Removed tj social adds

--- projects/tj-social-ads/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of tj-social-ads
- */
-
-export * from './lib/tj-social-ads.service';
-export * from './lib/tj-social-ads.component';
-export * from './lib/tj-social-ads.module';
