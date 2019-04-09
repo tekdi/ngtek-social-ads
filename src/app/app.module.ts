274a4cd714e0fc774f6022abdc3fd363d0049363
Ekta Y <ekta_y@tekditechnologies.com>
2019-04-09 16:49:02 +0530

    Improvement: Add Rotation for multiple ads #6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SocialAdsModule } from 'social-ads';
-
 @NgModule({
   declarations: [
     AppComponent
